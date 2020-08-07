810996067fab3e8cd01d3bb8aeec0da287899442
Johnie <ducl7023@gmail.com>
2020-08-07 18:37:23 +0900

    feat: initialize document read api with spring boot intialzr
    - 서버 베이스 코드를 가져왔습니다.

---
+++ api/document-read-api/src/main/java/com/jachwirus/documentreadapi/DocumentReadApiApplication.java
@@ -0,0 +1,13 @@
+package com.jachwirus.documentreadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentReadApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocumentReadApiApplication.class, args);
+    }
+
+}
---
+++ api/document-read-api/src/test/java/com/jachwirus/documentreadapi/DocumentReadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jachwirus.documentreadapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentReadApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
