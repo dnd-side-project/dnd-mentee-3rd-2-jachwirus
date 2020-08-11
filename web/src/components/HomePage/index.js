2789d8776f999e37a9069251618cb5ce42602c44
Rina <computer7214@gmail.com>
2020-08-11 22:15:24 +0900

    feat: create homepage
    홈 페이지의 검색창, 인기 꿀팁, 최근 변경 컴포넌트를 생성하였습니다.
    warning이 아주 많습니다,,, :(

--- web/src/components/HomePage/index.js
+++ web/src/components/HomePage/index.js
@@ -1,13 +1,33 @@
-import React, {Component} from 'react';
+import React from 'react';
+import GoodTip from '../GoodTip';
+import RecentChange from '../RecentChange'
+import './index.css';
+import findImage from '../../images/magnifying-glass.png';
 
-class HomePage extends Component {
-    render() {
-      return (
-        <header>
-          <h1>Home Page</h1>
-        </header>
-      );
-    }
-  }
+function HomePage() {
+  return (
+    <header className="HomePage">
+      <form action="/find_process" method="post"
+        onSubmit={function(e){
+            e.preventDefault();
+            //debugger;
+            // this.props.onSubmit(
+            //     e.target.find.value,
+            //     e.target.desc.value
+            // );
+        }.bind(this)}
+        className="Container"
+        id="find"
+      >
+        <input className="Find" type="text" name="find" placeholder="해시태그 검색"></input>
+        <input className="FindButton" type="image" src={findImage} alt="submit"></input>
+      </form>
+      <p className="Container" id="post">
+        <GoodTip/>
+        <RecentChange/>
+      </p>
+    </header>
+  );
+}
 
 export default HomePage;
