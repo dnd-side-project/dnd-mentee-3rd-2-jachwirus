164d3fc92734868a39be714fe1dc7a5acba7332f
Johnie <ducl7023@gmail.com>
2020-08-13 16:46:41 +0900

    fix: fix mock data load only at dev env
    - 개발환경에서만 mock 데이터를 삽입하는 방법으로 변경하였습니다.

--- api/document-read-api/src/main/java/com/jachwirus/documentreadapi/config/LoadDatabase.java
+++ api/document-read-api/src/main/java/com/jachwirus/documentreadapi/config/LoadDatabase.java
@@ -7,29 +7,48 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class LoadDatabase {
+    private final Environment environment;
+    public LoadDatabase(Environment environment){
+        this.environment = environment;
+    }
+
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
     @Bean
     CommandLineRunner initDatabase(DocumentRepository repository) {
-        return args -> {
-            String title,dataURL, thumbnailURL, category;
-            int like, dislike, view_count;
-            long last_version_id;
-
-            title = "tmp1"; dataURL="tmp1.com"; like=1; dislike=0;
-            thumbnailURL="tmp1Thumnail.com"; last_version_id=0;
-            view_count = 12; category="laundry";
-            repository.save(new Document(title, dataURL, like, dislike, thumbnailURL, last_version_id, view_count, category));
-
-            title = "tmp2"; dataURL="tmp2.com"; like=11; dislike=4;
-            thumbnailURL="tmp2Thumnail.com"; last_version_id=6;
-            view_count = 78; category="cafe";
-            repository.save(new Document(title, dataURL, like, dislike, thumbnailURL, last_version_id, view_count, category));
-
-            repository.findAll().forEach(doc -> log.info("Preloaded" + doc));
-        };
+        final String env = environment.getActiveProfiles()[0];
+        switch (env){
+            case "dev":
+                return args -> loadMockData(repository);
+            default:
+                return args -> {};
+        }
     }
+
+    private void loadMockData(DocumentRepository repository){
+        String title,dataURL, thumbnailURL, category;
+        int like, dislike, view_count;
+        long last_version_id;
+
+        title = "tmp1"; dataURL="tmp1.com"; like=1; dislike=0;
+        thumbnailURL="tmp1Thumnail.com"; last_version_id=0;
+        view_count = 12; category="laundry";
+        repository.save(
+                new Document(title, dataURL, like, dislike, thumbnailURL, last_version_id, view_count, category)
+        );
+
+        title = "tmp2"; dataURL="tmp2.com"; like=11; dislike=4;
+        thumbnailURL="tmp2Thumnail.com"; last_version_id=6;
+        view_count = 78; category="cafe";
+        repository.save(
+                new Document(title, dataURL, like, dislike, thumbnailURL, last_version_id, view_count, category)
+        );
+
+        repository.findAll().forEach(doc -> log.info("Preloaded" + doc));
+    }
+
 }
