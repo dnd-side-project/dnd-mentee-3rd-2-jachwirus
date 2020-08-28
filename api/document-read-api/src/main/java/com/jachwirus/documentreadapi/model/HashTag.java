9280058fd742f9ff69a1f6f154f398dc1d1fb5e9
Johnie <ducl7023@gmail.com>
2020-08-28 18:05:38 +0900

    feat: add index on hash tag name
    - 해시태그 기반 검색을 위한 인덱스를 추가하였습니다.

--- api/document-read-api/src/main/java/com/jachwirus/documentreadapi/model/HashTag.java
+++ api/document-read-api/src/main/java/com/jachwirus/documentreadapi/model/HashTag.java
@@ -10,11 +10,14 @@ import java.util.List;
 @Entity
 @Data
 @Accessors(chain=true)
+@Table(indexes = {@Index(name = "hash_tag_index", columnList = "tag_name", unique = true)})
 public class HashTag {
     @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
     @OneToMany(mappedBy = "hashTag")
     private List<DocumentHashTag> documents = new ArrayList<>();
+
+    @Column(name="tag_name")
     private String tagName;
 }
